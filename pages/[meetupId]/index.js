bb773e8a17943dfa279c7eed216290c0db3e8b29
johnnymcoy <cbuccioljr@gmail.com>
2022-12-11 17:24:08 +1100

    Fixed Fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -32,7 +32,7 @@ export async function getStaticPaths(){
     //dynamicaly generate paths 
     //this is temp 
     return{
-        fallback: false,
+        fallback: "blocking",
         paths: meetups.map(meetup => ({
             params: {meetupId: meetup._id.toString()}
         }))
